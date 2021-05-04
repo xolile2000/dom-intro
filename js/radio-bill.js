7e688fef0213b2cbd382762c8a1929f90ca1c5e4
xolile2000 <xolilentanzi43@gmail.com>
2021-05-04 15:51:14 +0200

    update

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,11 +1,48 @@
 // get a reference to the sms or call radio buttons
-
+var billItemTypeRadio = document.querySelector(".billItemTypeRadio")
 //get a reference to the add button
+var radioBillAddBtn = document.querySelector(".radioBillAddBtn")
 
+var callTotalTwo = document.querySelector(".callTotalTwo")
+
+var smsTotalTwo = document.querySelector(".smsTotalTwo")
+
+var totalTwo = document.querySelector(".totalTwo")
 //create a variable that will keep track of the total bill
+var trackTotalCall = 0
+var trackTotalSms = 0
+function radioBill(){
+    var radioBillAddBtn = document.querySelector("input[name='billItemType']:checked");
+    if (radioBillAddBtn){
+        var billItemType = radioBillAddBtn.value
+        // billItemType will be 'call' or 'sms'
+        if(billItemType=== "call"){
+            trackTotalCall += 2.75;
+        }
+        else if(billItemType==="sms"){
+            trackTotalSms += 0.75
+        }
+    }
+    
 
-//add an event listener for when the add button is pressed
+    callTotalTwo.innerHTML = trackTotalCall.toFixed(2);
+    smsTotalTwo.innerHTML = trackTotalSms.toFixed(2);
+    var totalCost = trackTotalCall + trackTotalSms
+    totalTwo.innerHTML = totalCost.toFixed(2);
 
+    totalTwo.classList.remove("danger");
+    totalTwo.classList.remove("warning");
+    
+    if(totalCost >= 30){
+        totalTwo.classList.add("danger")
+    }
+  else if(totalCost >= 20){
+    totalTwo.classList.add("warning")
+  } 
+
+}
+radioBillAddBtn.addEventListener('click', radioBill);
+// alert(radioBillAddBtn)
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the running total
 // * add nothing for invalid values that is not 'call' or 'sms'.
